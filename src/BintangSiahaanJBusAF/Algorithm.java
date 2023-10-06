4a0419410d9cb7a359414dcf12078b3cf752529d
bintangsiahaan <bintang.siahaan21@ui.ac.id>
2023-10-06 11:50:45 +0700

    CS05 - Menyelesaikan CS Modul 5

--- src/BintangSiahaanJBusAF/Algorithm.java
+++ src/BintangSiahaanJBusAF/Algorithm.java
@@ -1,5 +1,9 @@
 package bintangSiahaanJBusAF;
 import java.util.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.*;
 import java.util.stream.Collector;
 
@@ -7,6 +11,91 @@ public class Algorithm {
     private Algorithm(){
 
     }
+
+    //Method collect
+    public static <T> List<T> collect(Iterable<T> iterable, Predicate<T> pred){
+        List<T> result = new ArrayList<>();
+        for (T current : iterable) {
+            if (pred.predicate(current)) {
+                result.add(current);
+            }
+        }
+        return result;
+    }
+    public static <T> List<T> collect(Iterable<T> iterable, T value){
+        for(T current : iterable){
+            if(current == value){
+                return (List<T>) current;
+            }
+        }
+        return null;
+    }
+    public static <T> List<T> collect(T[] array, T value){
+        return collect(array, item -> item.equals(value));
+    }
+    public static <T> List<T> collect(T[] array, Predicate<T> pred){
+        List<T> result = new ArrayList<>();
+        for (T current : array) {
+            if (pred.predicate(current)) {
+                result.add(current);
+            }
+        }
+        return result;
+    }
+    public static <T> List<T> collect(Iterator<T> iterator, T value){
+        List<T> result = new ArrayList<>();
+        while (iterator.hasNext()) {
+            T current = iterator.next();
+            if (value.equals(current)) {
+                result.add(current);
+            }
+        }
+        return result;
+    }
+    public static <T> List<T> collect(Iterator<T> iterator, Predicate<T> pred){
+        List<T> result = new ArrayList<>();
+        while (iterator.hasNext()) {
+            T current = iterator.next();
+            if (pred.predicate(current)) {
+                result.add(current);
+            }
+        }
+        return result;
+    }
+
+
+    //Method count
+    public static <T> int count(Iterator<T> iterator, T value){
+        Predicate<T> predicate = value::equals;
+        return count(iterator, predicate);
+    }
+    public static <T> int count(T[] array, T value){
+        Iterator<T> iterator = Arrays.stream(array).iterator();
+        Predicate<T> predicate = value::equals;
+        return count(iterator, value);
+    }
+    public static <T> int count(Iterable<T> iterable, Predicate<T> predicate){
+        Iterator <T> iterator = iterable.iterator();
+        return count(iterator, predicate);
+    }
+    public static <T> int count(T[] array, Predicate<T> predicate){
+        Iterator<T> iterator = Arrays.stream(array).iterator();
+        return count(iterator, predicate);
+    }
+    public static <T> int count(Iterator<T> iterator, Predicate<T> predicate){
+        int counter = 0;
+        while(iterator.hasNext()){
+            if(predicate.predicate(iterator.next())) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public static <T> int count(Iterable<T> iterable, T value){
+        Predicate<T> predicate = value::equals;
+        return count(iterable, predicate);
+    }
+
     //Method exists
     public static <T> boolean exists(T[] array, T value){
         final Iterator<T> it = Arrays.stream(array).iterator();
@@ -41,4 +130,56 @@ public class Algorithm {
         }
         return false;
     }
+
+    //Method find
+    public static <T> T find(T[] array, T value) {
+        for(T i : array){ //For each i in array T compared with value
+            if(i == value){
+                return i;
+            }
+        }
+        return null;
+    }
+    public static <T> T find(Iterable<T> iterable, T value) {
+        for(T i : iterable){//For each i in iterable T compared with value
+            if(i == value){
+                return i;
+            }
+        }
+        return null;
+    }
+    public static <T> T find(Iterator<T> iterator, T value) {
+        while (iterator.hasNext()){
+            if(iterator.next() == value){
+                return iterator.next();
+            }
+        }
+        return null;
+    }
+    public static <T> T find(T[] array, Predicate<T> pred) {
+        for(T i : array){ //For each i in array T, predicate i
+            if(pred.predicate(i)){
+                return i;
+            }
+        }
+        return null;
+    }
+    public static <T> T find(Iterable<T> iterable, Predicate<T> pred) {
+        for(T i : iterable){//For each i in iterable T, predicate i
+            if(pred.predicate(i)){
+                return i;
+            }
+        }
+        return null;
+    }
+    public static <T> T find(Iterator<T> iterator, Predicate<T> pred) {
+        while (iterator.hasNext()){
+            if(pred.predicate(iterator.next())){
+                return iterator.next();
+            }
+        }
+        return null;
+    }
+
+
 }
--- src/BintangSiahaanJBusAF/JBus.java
+++ src/BintangSiahaanJBusAF/JBus.java
@@ -1,5 +1,7 @@
 package bintangSiahaanJBusAF;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Scanner;
 import java.sql.Timestamp;
 /**
@@ -15,18 +17,58 @@ import java.sql.Timestamp;
         return bus;
     }
     public static void main(String[] args) {
-      Integer[] numbers = {10, 20, 30, 40, 50};
-      int valueToCheck = 30;
+        Integer[] numbers = {18, 10, 22, 43, 18, 67, 12, 11, 88, 22, 18};
+        System.out.println("Number "+ Arrays.toString(numbers));
 
-      boolean result = Algorithm.exists(numbers, valueToCheck);
-      if (result) {
-        System.out.println(valueToCheck + " terdapat dalam array.");
-      } else{
-          System.out.println(valueToCheck + " tidak terdapat dalam array.");
-      }
+        // Tes Algorithm
+        System.out.print("1. ");
+        testCount(numbers);
+        System.out.print("2. ");
+        testFind(numbers);
+        System.out.print("3. ");
+        testExist(numbers);
+        System.out.println("4. Filtering");
+        testCollect(numbers);
+    }
+    private static void testExist(Integer[] t) {
+        int valueToCheck = 67;
+        boolean result3 = Algorithm.exists(t, valueToCheck);
+        if (result3) {
+            System.out.println(valueToCheck + " exist in the array.");
+        } else {
+            System.out.println(valueToCheck + " doesn't exists in the array.");
+        }
+    }
+    public static void testCount(Integer[] t) {
+        int valueToCount = 18;
+        int result = Algorithm.count(t, valueToCount);
+        System.out.println("Number " + valueToCount + " appears " + result + " times");
+    }
+    public static void testFind(Integer[] t) {
+        Integer valueToFind = 69;
+        Integer result2 = Algorithm.find(t, valueToFind);
+        System.out.print("Finding " + valueToFind + " inside the array : ");
+        if (result2 != null) {
+            System.out.println("Found!" + result2);
+        } else {
+            System.out.println("Not Found");
+        }
+    }
+    private static void testCollect(Integer[] t) {
+        Predicate<Integer> below = (val)->val<=22;
+        Predicate<Integer> above = (val)->val>43;
+
+        List<Integer> integerBelow = Algorithm.collect(t, below);
+        List<Integer> integerAbove = Algorithm.collect(t, above);
+
+        System.out.println("Below 22");
+        System.out.println(integerBelow);
+        System.out.println("Above 43");
+        System.out.println(integerAbove);
+    }
     /*Price[] unfilteredArray = new Price[5];
     for(int i = 0; i < unfilteredArray.length; i++){
-    
+
         int j = 5000;
         unfilteredArray[i] = new Price((i+1)*j);
     }
@@ -34,27 +76,27 @@ import java.sql.Timestamp;
     for(Price price : unfilteredArray){
         System.out.println(price.price);
     }
-    
+
     System.out.println("Below 12000.0");
     System.out.println(Validate.filter(unfilteredArray, 12000, true));
     System.out.println("Above 10000.0");
     System.out.println(Validate.filter(unfilteredArray, 10000, false));
 
-    
+
     Bus testBus = createBus();
-    
+
     //Payment
     Payment testPayment = new Payment(1, 1, 1, testBus.id, "S1");
     System.out.println(testPayment.getDepartureInfo());
     System.out.println(testPayment.getTime());
-    
+
     //test Schedule
     Calendar schedule1 = Calendar.getInstance();
     testBus.addSchedule(schedule1);
     Calendar schedule2 = Calendar.getInstance();
     schedule2.add(Calendar.DAY_OF_MONTH, 3);
     testBus.addSchedule(schedule2);
-    
+
     for(Schedule s : testBus.schedules){
         testBus.printSchedule(s);
     }
@@ -64,26 +106,26 @@ import java.sql.Timestamp;
     b.addSchedule(schedule1,12);
     b.addSchedule(schedule2,12);
     b.schedules.forEach(Schedule::printSchedule);
-    
+
     //Invalid date
     Timestamp t1 = Timestamp.valueOf("2023-7-19 15:00:00");
     System.out.println("Make booking at July 19, 2023 15:00:00 Seat AF12");
     System.out.println(Payment.makeBooking(t1, "AF12", b));
-    
+
     //Valid date, invalid seat
     Timestamp t2 = Timestamp.valueOf("2023-7-18 15:00:00");
     System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat AF20");
     System.out.println(Payment.makeBooking(t2, "AF07", b));
-    
+
     Timestamp t3 = Timestamp.valueOf("2023-7-20 12:00:00");
     System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat AF01");
     System.out.println(Payment.makeBooking(t3, "AF01", b));
-    
+
     System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat AF01 again");
     System.out.println(Payment.makeBooking(t3, "AF01", b));
-    
+
     //check if the data changed
     System.out.println("\nUpdated Schedule\n");
     b.schedules.forEach(Schedule::printSchedule);*/
-}
+
 }
